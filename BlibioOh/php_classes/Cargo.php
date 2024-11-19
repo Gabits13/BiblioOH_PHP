0c0f254cdd32b21f89cf6d992cc59831ad779a20
Gabriel Santos <131888357+Gabits13@users.noreply.github.com>
2024-11-19 15:57:46 -0300

    menus encaminhados e fale conosco

---
+++ BlibioOh/php_classes/Cargo.php
@@ -0,0 +1,157 @@
+<?php
+
+include_once 'conectar.php';
+
+class Cargo 
+{
+    private $codCargo;
+    private $descricao;
+    private $NomeCargo;
+    private $salario;
+    private $conn;
+
+    public function getCodCargo()
+    {
+        return $this->codCargo;
+    }
+    public function setCodCargo($codCargo)
+    {
+        $this->codCargo = $codCargo;
+    }
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+    }
+    public function getNomeCargo()
+    {
+        return $this->NomeCargo;
+    }
+    public function setNomeCargo($NomeCargo)
+    {
+        $this->NomeCargo = $NomeCargo;
+    } 
+    public function getSalario()
+    {
+        return $this->salario;
+    } 
+    public function setSalario($salario)
+    {
+        $this->salario = $salario;
+    }
+
+    function salvar() 
+    {
+        try {
+            $this->conn = new Conectar();
+            $sql = $this->conn->prepare("insert into cargo value (null,?,?,?)");
+            @$sql->bindParam(1, $this->getDescricao(), PDO::PARAM_STR);
+            @$sql->bindParam(2, $this->getNomeCargo(), PDO::PARAM_STR);
+            @$sql->bindParam(3, $this->getSalario(), PDO::PARAM_STR);
+            if ($sql->execute() == 1) 
+            {
+                return "Registro salvo com sucesso!";
+            }
+            $this->conn = null;
+        } 
+        catch (PDOException $exc) 
+        {
+            echo "Erro ao salvar o registro." . $exc->getMessage();
+        }
+    }
+
+    function alterar() 
+    {
+        try {
+            $this->conn = new Conectar();
+            $sql = $this->conn->prepare("select * from cargo where Cod_Cargo = ?");
+            @$sql->bindParam(1, $this->getCodCargo(), PDO::PARAM_STR);
+            $sql->execute();
+            return $sql->fetchAll();
+            $this->conn = null;
+        } 
+        catch (PDOException $exc) 
+        {
+            echo "Erro ao alterar." . $exc->getMessage();
+        }
+    }
+
+    function alterar2() 
+    {
+        try {
+            $this->conn = new Conectar();
+            $sql = $this->conn->prepare("update cargo set Descricao=?,Nome_Cargo=?,Salario=? where Cod_Cargo=?");
+            @$sql->bindParam(1, $this->getDescricao(), PDO::PARAM_STR);
+            @$sql->bindParam(2, $this->getNomeCargo(), PDO::PARAM_STR);
+            @$sql->bindParam(3, $this->getSalario(), PDO::PARAM_STR);
+            @$sql->bindParam(4, $this->getCodCargo(), PDO::PARAM_STR);
+            if ($sql->execute() == 1) 
+            {
+                return "Registro alterado com sucesso!";
+            }
+            $this->conn = null;
+        } 
+        catch (PDOException $exc) 
+        {
+            echo "Erro ao salvar o registro." . $exc->getMessage();
+        }
+    }
+
+    function consultar() 
+    {
+        try {
+            $this->conn = new Conectar();
+            $sql = $this->conn->prepare("select * from cargo where Nome_Cargo like ?");
+            @$sql->bindParam(1, $this->getNomeCargo(), PDO::PARAM_STR);
+            $sql->execute();
+            return $sql->fetchAll();
+            $this->conn = null;
+        } 
+        catch (PDOException $exc) 
+        {
+            echo "Erro ao consultar." . $exc->getMessage();
+        }
+    }
+
+    function excluir() 
+    {
+        try {
+            $this->conn = new Conectar();
+            $sql = $this->conn->prepare("delete from cargo where Cod_Cargo=?");
+            @$sql->bindParam(1, $this->getCodCargo(), PDO::PARAM_STR);
+            if ($sql->execute() == 1) 
+            {
+                return "Registro excluido com sucesso!";
+            }
+            else
+            {
+                return "Erro na esclusão !";
+            }
+            $this->conn = null;
+        } 
+        catch (PDOException $exc) 
+        {
+            echo "Erro ao excluir o registro." . $exc->getMessage();
+        }
+    }
+
+    function listar() 
+    {
+        try {
+            $this->conn = new Conectar();
+            $sql = $this->conn->prepare("select * from cargo order by Cod_Cargo");
+            $sql->execute();
+            return $sql->fetch();
+            $this->conn = null;
+        }
+        catch (PDOException $exc) 
+        {
+            echo "Erro ao executar consulta." . $exc->getMessage();
+        }
+    }
+}
+
+?>
---
+++ BlibioOh/php_classes/Setor.php
@@ -0,0 +1,150 @@
+<?php
+
+include_once 'conectar.php';
+
+class Setor {
+    private $codSetor;
+    private $andar;
+    private $genero;
+    private $conn;
+
+    public function getCodSetor()
+    {
+        return $this->codSetor;
+    }
+    public function setCodSetor($codSetor)
+    {
+        $this->codSetor = $codSetor;
+    }
+    public function getAndar()
+    {
+        return $this->andar;
+    }
+    public function setAndar($andar)
+    {
+        $this->andar = $andar;
+    } 
+    public function getGenero()
+    {
+        return $this->genero;
+    }
+    public function setGenero($genero)
+    {
+        $this->genero = $genero;
+    }
+
+    function salvar() 
+    {
+        try {
+            $this->conn = new Conectar();
+            $sql = $this->conn->prepare("insert into setor value(null,?,?)");
+            @$sql->bindParam(1, $this->getAndar(), PDO::PARAM_STR);
+            @$sql->bindParam(2, $this->getGenero(), PDO::PARAM_STR);
+            if ($sql->execute() == 1) 
+            {
+                return "Registro Salvo com sucesso!";
+            }
+            $this->conn = null;
+        } 
+        catch (PDOException $exc) 
+        {
+            echo "Erro ao salvar o registro." . $exc->getMessage();
+        }
+    }
+
+    function alterar()
+    {
+        try 
+        {
+            $this->conn = new Conectar();
+            $sql = $this->conn->prepare("select * from setor where Cod_Setor = ?");
+            @$sql->bindParam(1, $this->getCodSetor(), PDO::PARAM_STR);
+            $sql->execute();
+            return $sql->fetchAll();
+            $this->conn = null;
+        } 
+        catch (PDOException $exc) 
+        {
+            echo "Erro ao alterar." . $exc->getMessage();
+        }
+    }
+
+    function alterar2() 
+    {
+        try 
+        {
+            $this->conn = new Conectar();
+            $sql = $this->conn->prepare("update setor set Andar = ?, Genero = ? where Cod_Setor = ?");
+            @$sql->bindParam(1, $this->getAndar(), PDO::PARAM_STR);
+            @$sql->bindParam(2, $this->getGenero(), PDO::PARAM_STR);
+            @$sql->bindParam(3, $this->getCodSetor(), PDO::PARAM_STR);
+            if ($sql->execute() == 1) 
+            {
+                return "Alterado com sucesso!";
+            }
+            $this->conn = null;
+        }
+        catch (PDOException $exc) 
+        {
+            echo "Erro ao salvar o registro." . $exc->getMessage();
+        }
+    }
+
+    function consultar()
+    {
+        try 
+        {
+            $this->conn = new Conectar();
+            $sql = $this->conn->prepare("select * from setor where Genero like ?");
+            @$sql->bindParam(1, $this->getGenero(), PDO::PARAM_STR);
+            $sql->execute();
+            return $sql->fetchAll();
+            $this->conn = null;
+        } 
+        catch (PDOException $exc) 
+        {
+            echo "Erro ao consultar." . $exc->getMessage();
+        }
+    }
+
+    function excluir()
+    {
+        try 
+        {
+            $this->conn = new Conectar();
+            $sql = $this->conn->prepare("delete from setor where Cod_Setor = ?");
+            @$sql->bindParam(1, $this->getCodSetor(), PDO::PARAM_STR);
+            if ($sql->execute() == 1) 
+            {
+                return "Excluido com sucesso!";
+            }
+            else
+            {
+                return "Erro na exclusão!";
+            }
+            $this->conn = null;
+        } 
+        catch (PDOException $exc) 
+        {
+            echo "Erro ao excluir." . $exc->getMessage();
+        }
+    }
+
+    function listar()
+    {
+        try 
+        {
+            $this->conn = new Conectar();
+            $sql = $this->conn->prepare("select * from setor order by Andar");
+            $sql->execute();
+            return $sql->fetchAll();
+            $this->conn = null;
+        } 
+        catch (PDOException $exc) 
+        {
+            echo "Erro ao listar." . $exc->getMessage();
+        }
+    }
+}
+
+?>
